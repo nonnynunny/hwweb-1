579947acf00503345ed0186420556b813a4b77d6
nonnynunny <135826781+nonnynunny@users.noreply.github.com>
2023-06-27 14:38:23 +0700

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className='MyFooter'>
+            <span>Copyright 2023 @ Suttinee Sarik</span>
+        </div>
+    );
+}
+
+export default Footer;
